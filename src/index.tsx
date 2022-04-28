1c64a16b58c313545e1bc07b4a4b1983d6735790
Kyntaz <1260pedroquintas@gmail.com>
2022-04-28 13:00:39 +0100

    Add Bootstrap to index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { App } from './App/App';
+import { App } from './UI/App/App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 const rootElement = document.getElementById('root');
 
