6dcac9fd5748c9510b9ae1a998ceb714c9168402
Kyntaz <1260pedroquintas@gmail.com>
2022-05-02 00:18:32 +0100

    Implement UrlHandler

---
+++ src/UrlHandler/UrlHandler.ts
@@ -0,0 +1,48 @@
+import { SessionRecord } from "../Model/Records";
+import { Session } from "../Model/Session";
+
+const DEFAULT_SHARE_SESSION_PARAM = "session";
+
+export class UrlHandler {
+    private parameter = DEFAULT_SHARE_SESSION_PARAM;
+    private getWindowUrl = () => window.location.href;
+    private getWindowUrlSearch = () => window.location.search;
+    private ctrlC = (text: string) => navigator.clipboard.writeText(text);
+
+    public getAppUrl() {
+        const [appUrl] = this.getWindowUrl().split("?");
+        return appUrl;
+    }
+
+    public getParameters() {
+        return new URLSearchParams(this.getWindowUrlSearch());
+    }
+
+    public toShareableUrl(session: Session) {
+        const queryParameters = new URLSearchParams({
+            [this.parameter]: JSON.stringify(session.toRecord()),
+        }).toString();
+
+        return `${this.getAppUrl()}?${queryParameters}`
+    }
+
+    public writeShareableUrlToClipboard(session: Session) {
+        const url = this.toShareableUrl(session);
+        this.ctrlC(url);
+    }
+
+    public urlHasSharedSession() {
+        return !!this.getParameters().get(this.parameter);
+    }
+
+    public getSharedSession() {
+        const shared = this.getParameters().get(this.parameter);
+
+        if (!shared) {
+            throw new Error("No shared session");
+        }
+
+        const record: SessionRecord = JSON.parse(shared);
+        return Session.fromRecord(record);
+    }
+}
